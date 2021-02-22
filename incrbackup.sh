8bd7bcb6186c84ec8e283aa61744f1ebdd6fb3b5
nicholsonz <77360596+nicholsonz@users.noreply.github.com>
2021-02-22 09:56:35 -0600

    Update incrbackup.sh

--- incrbackup.sh
+++ incrbackup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # Sript to backup personal files to an external USB drive.
 #
 ########################################################
@@ -12,21 +13,7 @@ echo "  Start Backup $(date)"
 echo "############################################"
 echo
 
-# Check whether target volume is mounted, and mount it if not.
-
-#if ! mountpoint -q ${MNTPNT}/; then
-#	echo "Mounting the external USB drive."        
-#	echo "Mountpoint is ${MNTPNT}"
-#	if ! mount /dev/sdb1 ${MNTPNT}; then
-#		echo "An error code was returned by mount command!"
-#		exit 5
-#	else echo "Mounted successfully.";
-#	fi
-#else echo "${MNTPNT} is already mounted.";
-#fi
-
-
-if ! mountpoint -q ${MNTPNT}/; then
+if [ ! -d $MNTPNT ]; then
 	echo "Drive not mounted! Cannot run backup without backup volume!"
 	exit 1
 fi
@@ -36,24 +23,25 @@ echo
 echo "--------- Starting backup of /home . . . ----------"
 echo
 
-# Create the target directory path if it does not already exist.
+
 mkdir --parents ${BACKUP_PATH}/home
 
-# backup using rsync
-sudo rsync --perms --archive --verbose --human-readable --itemize-changes --progress --delete --delete-excluded --exclude='.dbus' --exclude='Examples' --exclude='.local' --exclude='.thumbnails' --exclude='transient-items' --exclude='.cache' --exclude='Steam' --exclude='.steam' --exclude='.zenmap' --exclude='.bash*' --exclude='mail/' /home ${BACKUP_PATH}/ 
+
+sudo rsync --perms --archive --verbose --human-readable --itemize-changes --delete --delete-excluded --exclude='.dbus' --exclude='Examples' --exclude='.local' --exclude='.thumbnails' --exclude='transient-items' --exclude='.cache' --exclude='Steam' --exclude='.steam' --exclude='.zenmap' --exclude='.bash*' --exclude='mail/' /home ${BACKUP_PATH}/ 
 
 echo
 echo "---------------------------------------------------"
 echo
 echo "--------- Starting backup of /etc . . . -----------"
 echo
-# Create the target directory path if it does not already exist.
+
+
 mkdir --parents ${BACKUP_PATH}/etc
 
 
-sudo rsync --perms --archive --verbose --human-readable --itemize-changes --progress --delete --delete-excluded /etc ${BACKUP_PATH}/ 
+sudo rsync --perms --archive --verbose --human-readable --itemize-changes --delete --delete-excluded /etc ${BACKUP_PATH}/ 
 
-# print staus of backup to screen and log file
+
 if test -e "${MNTPNT}/$(hostname)/rsync-output.log"; then
 	echo "$(date) Success!" >> "${MNTPNT}/$(hostname)/rsync-output.log"
 echo
@@ -65,6 +53,5 @@ else
 	touch ${MNTPNT}/$(hostname)/rsync-output.log 
 	echo "New log file created!"
 fi
-
 
 
