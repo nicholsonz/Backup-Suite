0b89c20d69e9c4a3a6456c58c27a99dbbea0b41b
nicholsonz <77360596+nicholsonz@users.noreply.github.com>
2022-01-15 14:30:18 -0600

    Update incrbackup.sh

--- incrbackup.sh
+++ incrbackup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-# Backup personl files/dirs to local drive
+# Sript to backup personal files to an external USB drive.
 #
-########################################################
+#
 MNTPNT='/mnt/backup'
 BACKUP_PATH=${MNTPNT}/$(hostname)
-#######################################################
 
 
 echo "############################################"
-echo "  Start Backup $(date)" 
+echo "Start Incremental Backup $(date)" 
 echo "############################################"
 echo
 
@@ -23,11 +22,8 @@ echo
 echo "--------- Starting backup of /home . . . ----------"
 echo
 
-
 mkdir --parents ${BACKUP_PATH}/home
-
-
-sudo rsync --perms --archive --verbose --human-readable --itemize-changes --delete --delete-excluded --exclude='.dbus' --exclude='Examples' --exclude='.local' --exclude='.thumbnails' --exclude='transient-items' --exclude='.cache' --exclude='Steam' --exclude='.steam' --exclude='.zenmap' --exclude='.bash*' --exclude='mail/' /home ${BACKUP_PATH}/ 
+rsync --perms --archive --verbose --human-readable --itemize-changes --delete-excluded --exclude='.cache' --exclude='Downloads/' /home ${BACKUP_PATH}/ 
 
 echo
 echo "---------------------------------------------------"
@@ -35,23 +31,42 @@ echo
 echo "--------- Starting backup of /etc . . . -----------"
 echo
 
-
 mkdir --parents ${BACKUP_PATH}/etc
+rsync --perms --archive --verbose --human-readable --itemize-changes --delete-excluded /etc ${BACKUP_PATH}/ 
 
 
-sudo rsync --perms --archive --verbose --human-readable --itemize-changes --delete --delete-excluded /etc ${BACKUP_PATH}/ 
+echo
+echo "---------------------------------------------------"
+echo
+echo "-------- Starting backup of /var . . . ------------"
+echo
 
+mkdir --parents ${BACKUP_PATH}/var
+rsync --perms --archive --verbose --human-readable --itemize-changes --progress --delete-excluded --exclude='.Trash-1000' --exclude='*.mp4' /var/www ${BACKUP_PATH}/var/
+
+echo
+echo "--------- Starting backup of /srv . . . ----------"
+echo
+
+mkdir --parents ${BACKUP_PATH}/srv
+rsync --perms --archive --verbose --human-readable --itemize-changes --delete-excluded /srv ${BACKUP_PATH}/
+
+echo
+echo "---------------------------------------------------"
+echo
+
+
+echo
+echo "##################################################"
+echo "Incremental Backup Completed! $(date)  "
+echo "##################################################" 
 
 if test -e "${MNTPNT}/$(hostname)/rsync-output.log"; then
 	echo "$(date) Success!" >> "${MNTPNT}/$(hostname)/rsync-output.log"
-echo
-echo "##################################################"
-echo "  Backup Completed! $(date)  "
-echo "##################################################" 
-
 else
 	touch ${MNTPNT}/$(hostname)/rsync-output.log 
-	echo "New log file created!"
+	echo "$(date) Success!" >> "${MNTPNT}/$(hostname)/rsync-output.log"
+        echo "New log file created!"
 fi
 
-exit 0
+exit 0 
